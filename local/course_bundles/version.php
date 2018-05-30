aba659019cc833376764cc9e55df6c43820f67ff
Erik <erik@pivotcreates.com>
2018-05-30 08:08:46 -0500

    added plugin to change the drawer navigation and pages to show courses completed and in progress.

--- local/course_bundles/version.php
+++ local/course_bundles/version.php
@@ -22,7 +22,7 @@
  
 defined('MOODLE_INTERNAL') || die();
  
-$plugin->version   = 2018050200.01;
+$plugin->version   = 2018050200.02;
 $plugin->requires  = 2017111300; // Requires this Moodle version (3.4).
 $plugin->cron      = 0;
 $plugin->component = 'local_course_bundles';
