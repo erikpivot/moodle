75fa57c811cdaef473c9db289ac5784bfcbbc6d8
Erik <erik@pivotcreates.com>
2018-05-31 07:38:35 -0500

    Updated enrollment times for the wordpress to moodle plugin.

--- auth/wp2moodle/login.php
+++ auth/wp2moodle/login.php
@@ -100,7 +100,9 @@ function enrol_into_course($courseid, $userid, $roleid = 5) {
 		MUST_EXIST
 	);
 	// retrieve enrolment instance associated with your course
-	return $manualenrol->enrol_user($enrolinstance, $userid, $roleid); // enrol the user
+	$timestart = strtotime(date());
+	$timeend = $timestart + 15724800;
+	return $manualenrol->enrol_user($enrolinstance, $userid, $roleid, $timestart, $timeend); // enrol the user
 }
 
 /**
@@ -392,7 +394,9 @@ if (!empty($_GET)) {
 				if ($DB->record_exists('course', array('idnumber'=>$course))) {
 					$courserow = $DB->get_record('course', array('idnumber'=>$course));
 					if (get_config('auth/wp2moodle', 'redirectnoenrol') !== 'yes') {
-						if (!enrol_try_internal_enrol($courserow->id, $user->id, $studentrow->id)) {
+					    $timestart = strtotime(date());
+	                    $timeend = $timestart + 15724800;
+						if (!enrol_try_internal_enrol($courserow->id, $user->id, $studentrow->id, $timestart, $timeend)) {
 							continue;
 						}
 					}
