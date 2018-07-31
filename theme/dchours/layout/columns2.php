28c69d873184abd3e4fcdd554adc318e72f413d6
Erik <erik@pivotcreates.com>
2018-07-31 11:55:18 -0500

    Added dchours theme files to tracking

---
+++ theme/dchours/layout/columns2.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A two column layout for the boost theme.
+ *
+ * @package   theme_dchours
+ * @copyright 2018 Pivot Creative
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
+require_once($CFG->libdir . '/behat/lib.php');
+
+if (isloggedin()) {
+    $navdraweropen = (get_user_preferences('drawer-open-nav', 'true') == 'true');
+} else {
+    $navdraweropen = false;
+}
+$extraclasses = [];
+if ($navdraweropen) {
+    $extraclasses[] = 'drawer-open-left';
+}
+
+if (is_siteadmin()) {
+    $extraclasses[] = 'pivot-site-admin';
+}
+
+$bodyattributes = $OUTPUT->body_attributes($extraclasses);
+$blockshtml = $OUTPUT->blocks('side-pre');
+$hasblocks = strpos($blockshtml, 'data-block=') !== false;
+$regionmainsettingsmenu = $OUTPUT->region_main_settings_menu();
+$templatecontext = [
+    'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
+    'output' => $OUTPUT,
+    'sidepreblocks' => $blockshtml,
+    'hasblocks' => $hasblocks,
+    'bodyattributes' => $bodyattributes,
+    'navdraweropen' => $navdraweropen,
+    'regionmainsettingsmenu' => $regionmainsettingsmenu,
+    'hasregionmainsettingsmenu' => !empty($regionmainsettingsmenu)
+];
+/*
+if (is_siteadmin()) {
+    // Add Simple Administration, if the user is an administrator
+    $url = new moodle_url('/simpleadmin.php');
+    $simpleadmin = navigation_node::create(get_string('simpleadmin', 'theme_dchours'), $url);
+    $flat = new flat_navigation_node($simpleadmin, 0);
+    $flat->set_showdivider(true);
+    $flat->key = 'simpleadmin';
+    $PAGE->flatnav->add($flat);   
+}
+*/
+$templatecontext['flatnavigation'] = $PAGE->flatnav; 
+echo $OUTPUT->render_from_template('theme_dchours/columns2', $templatecontext);
+
---
+++ theme/dchours/version.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * DC Hours
+ *
+ * @package   theme_dchours
+ * @copyright 2018 Pivot Creative
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();                                                                                                
+ 
+// This is the version of the plugin.                                                                                               
+$plugin->version = '2018073100.01';                                                                                                    
+ 
+// This is the version of Moodle this plugin requires.                                                                              
+$plugin->requires = '2017110800';                                                                                                   
+ 
+// This is the component name of the plugin - it always starts with 'theme_'                                                        
+// for themes and should be the same as the name of the folder.                                                                     
+$plugin->component = 'theme_dchours';                                                                                                 
+ 
+// This is a list of plugins, this plugin depends on (and their versions).                                                          
+$plugin->dependencies = [                                                                                                           
+    'theme_boost' => '2017111300'                                                                                                   
+];
