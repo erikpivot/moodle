0eeb275d0c803bd5993ef436bfc13ee5b3285d8b
Erik <erik@pivotcreates.com>
2019-01-22 06:09:04 -0800

    Removed logout confirmation

--- login/logout.php
+++ login/logout.php
@@ -44,14 +44,14 @@ if (!isloggedin()) {
     require_logout();
     redirect($redirect);
 
-} else if (!confirm_sesskey($sesskey)) {
+} /*else if (!confirm_sesskey($sesskey)) {
     $PAGE->set_title($SITE->fullname);
     $PAGE->set_heading($SITE->fullname);
     echo $OUTPUT->header();
     echo $OUTPUT->confirm(get_string('logoutconfirm'), new moodle_url($PAGE->url, array('sesskey'=>sesskey())), $CFG->wwwroot.'/');
     echo $OUTPUT->footer();
     die;
-}
+}*/
 
 $authsequence = get_enabled_auth_plugins(); // auths, in sequence
 foreach($authsequence as $authname) {
