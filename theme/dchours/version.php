4fc46552bce4aa751bd2bc8b195935449bfc029c
Erik <erik@pivotcreates.com>
2019-03-12 05:49:55 -0700

    updated dchours theme

--- theme/dchours/version.php
+++ theme/dchours/version.php
@@ -11,7 +11,7 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
  
 // This is the version of the plugin.                                                                                               
-$plugin->version = '2018080800.00';                                                                                                    
+$plugin->version = '2019031200.00';                                                                                                    
  
 // This is the version of Moodle this plugin requires.                                                                              
 $plugin->requires = '2017110800';                                                                                                   
