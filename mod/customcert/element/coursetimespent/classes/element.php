00350a07a1934e8826f7660578c144f28126bafa
Erik <erik@pivotcreates.com>
2019-04-11 08:33:43 -0700

    Another certificate fix for empty hours and minutes

--- mod/customcert/element/coursetimespent/classes/element.php
+++ mod/customcert/element/coursetimespent/classes/element.php
@@ -57,17 +57,17 @@ class element extends \mod_customcert\element {
         $formatted_time = explode(':', $time_res->value);
         $hours = ltrim($formatted_time[0], 0);
         $minutes = ltrim($formatted_time[1], 0);
-        // make sure the time spent in the course does not exceed the credit hours
-        if (($hours > $time_res->credithrs) || (empty($hours) && empty($minutes))) {
-            $hours = $time_res->credithrs;
-            $minutes = 0;
-        }
-        
         if (empty($hours)) {
             $hours = 0;
         }
         
         if (empty($minutes)) {
+            $minutes = 0;
+        }
+        
+        // make sure the time spent in the course does not exceed the credit hours
+        if (($hours > $time_res->credithrs) || (empty($hours) && empty($minutes))) {
+            $hours = $time_res->credithrs;
             $minutes = 0;
         }
         
